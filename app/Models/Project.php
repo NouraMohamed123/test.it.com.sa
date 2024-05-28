e30edd444220f3b383fa45427ad0b56e1fc95365
NouraMohamed123 <nouramohamed091@gmail.com>
2024-05-28 12:18:05 +0300

    ff

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,7 +12,7 @@ class Project extends Model
     protected $dates = ['deleted_at'];
 
     protected $fillable = [
-        'title','company_id','client_id','summary','start_date','end_date','priority',
+        'title','company_id','summary','start_date','end_date','priority',
         'description','project_progress','status','project_note',
 
     ];
